f6a50b656c9812d4939f048cf7651cee21eb66bb
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-02 14:44:22 +0100

    Building a route exercise 1

--- app.rb
+++ app.rb
@@ -7,4 +7,10 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/hello' do
+    name = params[:name]
+    
+    return "Hello #{name}"
+  end
 end
